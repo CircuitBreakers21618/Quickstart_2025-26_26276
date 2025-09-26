cb2d7394172e222b70e1032e002c5d86f36fa67d
wykzimme <bryce.e.zimmerman@gmail.com>
2025-09-26 17:15:07 -0600

    gggg

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/teleop.java
@@ -0,0 +1,35 @@
+package org.firstinspires.ftc.teamcode.pedroPathing;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp(name = "teleop")
+public class teleop extends OpMode {
+
+    DcMotor lf, rf;
+    DcMotor lr, rr;
+
+    @Override
+    public void init() {
+        lf = hardwareMap.get(DcMotor.class, "lf");
+        lr = hardwareMap.get(DcMotor.class, "lr");
+        rf = hardwareMap.get(DcMotor.class, "rf");
+        rr = hardwareMap.get(DcMotor.class, "rr");
+
+
+
+    }
+
+    @Override
+    public void loop() {
+        lf.setPower(-gamepad1.right_stick_y + -gamepad1.left_stick_x * 1.1 + gamepad1.right_stick_x);
+        lr.setPower(gamepad1.right_stick_y + -gamepad1.left_stick_x * 1.1 -gamepad1.right_stick_x);
+        rf.setPower(-gamepad1.right_stick_y + gamepad1.left_stick_x * 1.1 -gamepad1.right_stick_x);
+        rr.setPower(gamepad1.right_stick_y + gamepad1.left_stick_x * 1.1 + gamepad1.right_stick_x);
+
+
+
+    }
+}
