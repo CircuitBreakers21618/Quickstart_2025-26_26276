339e1311b5192ba65ff0e8990820b3f78273c908
wykzimme <bryce.e.zimmerman@gmail.com>
2025-11-03 18:31:16 -0700

    Update teleop.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/teleop.java
@@ -3,12 +3,17 @@ package org.firstinspires.ftc.teamcode.pedroPathing;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Gamepad;
 
 @TeleOp(name = "teleop good")
 public class teleop extends OpMode {
 
     DcMotor lf, rf;
     DcMotor lr, rr;
+    DcMotor shooter;
+    Gamepad currentGamepad2 = new Gamepad();
+    Gamepad previousGamepad2 = new Gamepad();
+    int intake = 0;
 
     @Override
     public void init() {
@@ -16,6 +21,7 @@ public class teleop extends OpMode {
         lr = hardwareMap.get(DcMotor.class, "lr");
         rf = hardwareMap.get(DcMotor.class, "rf");
         rr = hardwareMap.get(DcMotor.class, "rr");
+        shooter = hardwareMap.get(DcMotor.class, "shooter");
 
 
 
@@ -27,6 +33,26 @@ public class teleop extends OpMode {
         lr.setPower(gamepad1.right_stick_y + -gamepad1.left_stick_x * 1.1 -gamepad1.right_stick_x);
         rf.setPower(-gamepad1.right_stick_y + gamepad1.left_stick_x * 1.1 -gamepad1.right_stick_x);
         rr.setPower(gamepad1.right_stick_y + gamepad1.left_stick_x * 1.1 + gamepad1.right_stick_x);
+
+
+        if (currentGamepad2.b && !previousGamepad2.b) {
+            if (intake == 1) { //position 0 -> 1
+
+
+
+                shooter.setPower(1);
+
+
+                intake = 0;
+
+            } else if (intake == 0) { //position 1 -> 0
+
+                shooter.setPower(0);
+                intake = 1;
+
+            }
+        }
+
 
 
 
